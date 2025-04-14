fe7cd3995a882e7a3b0c9b96d62a360d194551af
IvanilovA147 <118895282+IvanilovA147@users.noreply.github.com>
2025-04-14 11:12:46 +0300

    заа

--- project_PPZ/news/urls.py
+++ project_PPZ/news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import register_view, login_view
 
+app_name = "news"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', login_view, name='login'),
+    path('register', register_view, name='register'),
 ]
--- project_PPZ/news/views.py
+++ project_PPZ/news/views.py
@@ -1,6 +1,35 @@
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import RegistrationForm, UserLoginForm
 
 
 def index(request):
     return render(request, "news/home.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('http://127.0.0.1:8000/')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'news/register.html', {"form": form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user =  authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('http://127.0.0.1:8000/')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {'form': form})
