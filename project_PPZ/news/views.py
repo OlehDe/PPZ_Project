13f2bede12a29dcaf7f3528782265eeb496b1e83
IvanilovA147 <118895282+IvanilovA147@users.noreply.github.com>
2025-04-14 10:27:26 +0300

    ppz

--- project_PPZ/news/views.py
+++ project_PPZ/news/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Привіт із додатку news! Це мій проект з ППЗ")
+    return render(request, "news/home.html")
